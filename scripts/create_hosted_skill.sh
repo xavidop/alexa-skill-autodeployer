489acea811835d0f63fad8727436d7dad7321237
Xavi <xavi_tb@hotmail.com>
2020-07-16 00:17:43 +0200

    commit and skill name modifications

--- scripts/create_hosted_skill.sh
+++ scripts/create_hosted_skill.sh
@@ -4,13 +4,13 @@ set template [lindex $argv 0];
 
 set timeout 6000
 
-spawn ask new --template-url $template
+spawn ask new 
 expect "Choose the programming language you will use to code your skill"
 send -- "\r"
 expect "Choose a method to host your skill's backend resources"
 send -- "\r"
 expect "Please type in your skill name:"
-send -- "\r"
+send -- "${template}\r"
 expect "Please type in your folder name for the skill project (alphanumeric):"
 send -- "template\r"
 expect "Hosted skill provisioning finished"
