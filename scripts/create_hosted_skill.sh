6489d759e5472cd0d81f487826d43847b54f2122
Xavi <xavi_tb@hotmail.com>
2020-07-15 22:48:23 +0200

    added new scripts

---
+++ scripts/create_hosted_skill.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/expect
+
+set template [lindex $argv 0];
+
+set timeout 6000
+
+spawn ask new --template-url $template
+expect "Choose the programming language you will use to code your skill"
+send -- "\r"
+expect "Choose a method to host your skill's backend resources"
+send -- "\r"
+expect "Please type in your skill name:"
+send -- "\r"
+expect "Please type in your folder name for the skill project (alphanumeric):"
+send -- "\r"
+expect "Hosted skill provisioning finished"
+
+
