844a47566aa8f24e13521d48fd85547501a6817b
Xavi <xavi_tb@hotmail.com>
2020-07-17 17:03:54 +0200

    change the deployment in v1

--- deploy_hosted_skill_v1.sh
+++ deploy_hosted_skill_v1.sh
@@ -5,8 +5,6 @@ set template [lindex $argv 0];
 set timeout 6000
 
 spawn ask deploy --force
-expect "Please type in your skill name"
-send -- "template\r"
 expect "Do you want to proceed with the above deployments"
 send -- "\r"
 expect "Alexa hosted skill is created. Do you want to clone the skill"
