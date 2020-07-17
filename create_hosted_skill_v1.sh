e1b4784ce4298ad9e3ab5dc284f427f5ebccfe91
Xavi <xavi_tb@hotmail.com>
2020-07-17 14:01:43 +0200

    multipe cli versions

---
+++ create_hosted_skill_v1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect
+
+set template [lindex $argv 0];
+
+set timeout 6000
+
+spawn ask create-hosted-skill
+expect "Please type in your skill name"
+send -- "template\r"
+expect "Please select the runtime"
+send -- "\r"
+expect "Alexa hosted skill is created. Do you want to clone the skill project to current working directory?"
+send -- "\r"
+expect "successfuly cloned."
+
+
