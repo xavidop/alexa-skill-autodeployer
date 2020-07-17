6a9024cbc1a569189d8771c8eff33d81eda0451b
Xavi <xavi_tb@hotmail.com>
2020-07-17 17:25:25 +0200

    error in deployment

--- deploy_hosted_skill_v1.sh
+++ deploy_hosted_skill_v1.sh
@@ -7,8 +7,6 @@ set timeout 6000
 spawn ask deploy --force
 expect "Do you want to proceed with the above deployments"
 send -- "\r"
-expect "Alexa hosted skill is created. Do you want to clone the skill"
-send -- "\r"
 expect "Your skill code deployment has started"
 
 
